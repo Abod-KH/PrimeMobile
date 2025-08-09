5f9919f80d8968c44e63013200ef8b4e4dc53619
Your Name <your.email@example.com>
2025-08-09 20:08:25 +0300

    complet payment

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-// import {
-//   createCheckoutSession,
-//   Metadata,
-// } from "@/actions/createCheckoutSession";
+import {
+  createCheckoutSession,
+  Metadata,
+} from "../actions/createCheckoutSession";
 import Container from "@/components/Container";
 import EmptyCart from "@/components/EmptyCart";
 import NoAccess from "@/components/NoAccess";
@@ -81,23 +81,23 @@ const CartPage = () => {
 
   const handleCheckout = async () => {
     setLoading(true);
-    // try {
-    //   const metadata: Metadata = {
-    //     orderNumber: crypto.randomUUID(),
-    //     customerName: user?.fullName ?? "Unknown",
-    //     customerEmail: user?.emailAddresses[0]?.emailAddress ?? "Unknown",
-    //     clerkUserId: user?.id,
-    //     address: selectedAddress,
-    //   };
-    //   const checkoutUrl = await createCheckoutSession(groupedItems, metadata);
-    //   if (checkoutUrl) {
-    //     window.location.href = checkoutUrl;
-    //   }
-    // } catch (error) {
-    //   console.error("Error creating checkout session:", error);
-    // } finally {
-    //   setLoading(false);
-    // }
+    try {
+      const metadata: Metadata = {
+        orderNumber: crypto.randomUUID(),
+        customerName: user?.fullName ?? "Unknown",
+        customerEmail: user?.emailAddresses[0]?.emailAddress ?? "Unknown",
+        clerkUserId: user?.id,
+        address: selectedAddress,
+      };
+      const checkoutUrl = await createCheckoutSession(groupedItems, metadata);
+      if (checkoutUrl) {
+        window.location.href = checkoutUrl;
+      }
+    } catch (error) {
+      console.error("Error creating checkout session:", error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="bg-gray-50 pb-52 md:pb-10">
